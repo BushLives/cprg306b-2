8c49ed188647fdac4d51cfcb69fd6c40d4233f4e
CodyDavidsonSD <cody.davidson1@edu.sait.ca>
2026-06-19 16:41:19 -0600

    adds footer and navbar content

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <header className='w-full py-4 bg-slate-800 flex flex-row items-center justify-between'>
+    <header className='w-full py-3 bg-slate-800 flex flex-row items-center justify-between'>
         <div className='px-8'>
             <span className='text-slate-100 font-semibold text-lg hover:text-slate-300 hover:underline'>Students</span>
+        </div>
+        <div className='px-8 flex flex-col items-center justify-center border-b border-slate-600'>
+            <span className='text-slate-100 font-bold text-2xl'>RAU</span>
+            <span className='text-sm text-slate-400 mb-0.5'>Rea Ahll University</span>
         </div>
         <div className='px-8'>
             <span className='text-slate-100 font-semibold text-lg hover:text-slate-300 hover:underline'>Account</span>
---
+++ app/components/SchoolInfo.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const SchoolInfo = () => {
+  return (
+    <footer className='flex flex-col items-center justify-end-safe py-2'>
+        <p className='text-sm font-semibold dark:text-mist-700 text-gray-400'>
+            Rea Ahll University
+        </p>
+        <p className='text-sm dark:text-mist-700 text-gray-400'>
+            100 Place Street, Township
+        </p>
+        <p className='text-sm dark:text-mist-700 text-gray-400'>
+            Alberta Canada
+        </p>
+    </footer>
+  )
+}
+
+export default SchoolInfo
