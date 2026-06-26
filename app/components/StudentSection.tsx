a077a899b8f5a75cdeca6ba4460248fafbe5e093
noahg1238 <noah.greaves@edu.sait.ca>
2026-06-26 15:33:14 -0600

    added Students to page

---
+++ app/components/StudentSection.tsx
@@ -0,0 +1,22 @@
+import data from "../../data/students.json";
+
+const StudentSection = () => {
+  return (
+    <div className="flex flex-wrap justify-center gap-6 lg:gap-8 justify-self-start sm:px-2">
+      {data.students.map((student) => (
+        <div
+          key={student.id}
+          className="bg-slate-800 rounded-4xl text-center p-6 w-60"
+        >
+          <h2 className="text-2xl text-white">
+            {student.firstName} {student.lastName}
+          </h2>
+          <p className="text-gray-500">{student.dob}</p>
+          <p className="text-white">Grade: {student.grade}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default StudentSection;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import StudentSection from "./components/StudentSection";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center font-sans">
-      
+    <div className="flex flex-col flex-1 items-center font-sans pt-12">
+      <StudentSection />
     </div>
   );
 }
