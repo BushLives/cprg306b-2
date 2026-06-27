fcad258772777c988558c1e1affea805a2e55173
noahg1238 <noah.greaves@edu.sait.ca>
2026-06-26 19:27:06 -0600

    added better data validation

--- app/lib/schema.tsx
+++ app/lib/schema.tsx
@@ -1,10 +1,10 @@
-import z, { int } from "zod";
+import z from "zod";
 
 export const addStudentSchema = z.object({
-    firstName: z.string().min(2, "Please enter your first name."),
-    lastName: z.string().min(2, "Please enter your last name."),
-    dateOfBirth: z.string(),
-    grade: z.string().max(2, "Please enter a letter grade")
+    firstName: z.string().min(2, "Please enter your first name (>2 chars)."),
+    lastName: z.string().min(2, "Please enter your last name (>2 chars)."),
+    dateOfBirth: z.coerce.date(),
+    grade: z.string().regex(/^(?:[A-D][+-]?|[F])$/, "Please enter a Letter grade from A-F with an optional + or -")
 })
 
 export type studentData = z.infer<typeof addStudentSchema>
