257acf760ee51926913ef013f34cfb749141b88c
CodyDavidsonSD <cody.davidson1@edu.sait.ca>
2026-06-26 18:04:19 -0600

    adds submission confirmation
    adds form submission confirmation and loading spinner

--- app/lib/schema.tsx
+++ app/lib/schema.tsx
@@ -3,8 +3,8 @@ import z, { int } from "zod";
 export const addStudentSchema = z.object({
     firstName: z.string().min(2, "Please enter your first name."),
     lastName: z.string().min(2, "Please enter your last name."),
-    dateOfBirth: z.date({}),
-    grade: z.int
+    dateOfBirth: z.string(),
+    grade: z.string().max(2, "Please enter a letter grade")
 })
 
 export type studentData = z.infer<typeof addStudentSchema>
